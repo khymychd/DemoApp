d04093e6018c21c5f7ea2c0ec3bfe01bf586951f
KillerBe <killerbe@jmail.com>
2020-12-14 21:51:44 +0200

    Initial Commit

---
+++ DemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Dima Khymych on 14.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
